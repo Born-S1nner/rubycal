0e3e17c7faa1750f9dbfea7325d722229ae344a5
123 <cookie.twenty01@gmail.com>
2021-04-21 16:57:08 -0400

    effect #6

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,2 +1,28 @@
+#t.integer "first_input"
+#t.integer "second_input"
+#t.integer "operation"
+#t.float "result"
+#t.integer "request_count"
+
 class Calculator < ApplicationRecord
+  enum operation: [ :sum_op, :difference_op, :multiplication_op, :division_op ]
+
+  def calculate
+    self.result = eval "#{Float(first_input)} #{operator} #{Integer(second_input)}" 
+    raise "Invalid result: #{result}" if result.nan? || result.infinite?
+  rescue => e
+    self.result = nil 
+    self.result_error = e
+  ensure
+    return result
+  end
+
+  def operator
+    case operation.to_sym
+    when :sum_op then '+'
+    when :difference_op then '-'
+    when :multiplication_op then '*'
+    when :division_op then '/'
+  end
+
 end
