81708808b6a77f90daa2d9b6b62d74d8465a94ad
123 <cookie.twenty01@gmail.com>
2021-04-22 17:01:42 -0400

    lucas #2

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,4 +1,14 @@
 class CalculatorController < ApplicationController
+  def create
+    @calculate = Calculator.find_or_initialize_by(calculate_params)
+    
+  end
   def index
+  end
+  
+  private
+  
+  def calculate_params
+    params.permit(:first_input, :second_input, :operation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :calculators
+  resources :calculator
   root "calculator#index"
 end
