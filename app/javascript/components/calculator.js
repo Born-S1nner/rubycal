5c486d3f931a4c6a3bcf9f0a01272b3973f1b235
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-04-27 15:37:15 -0400

    Update calculator.js

--- app/javascript/components/calculator.js
+++ app/javascript/components/calculator.js
@@ -52,30 +52,32 @@ class Calculator extends React.Component {
         <h3>Hello from React gang</h3>
         {this.renderInput('first_input', this.state.first_input)}
         {this.renderInput('second_input', this.state.second_input)}
-        <div className="row">
-          <Buttons display='7' value={7} click={this.numbers} />
-          <Buttons display='8' value={8} click={this.numbers} />
-          <Buttons display='9' value={9} click={this.numbers} />
-          <Buttons display='-' value='-' click={this.oper} />
-        </div>
-        <div className="row">
-          <Buttons display='4' value={4} click={this.numbers} />
-          <Buttons display='5' value={5} click={this.numbers} />
-          <Buttons display='6' value={6} click={this.numbers} />
-          <Buttons display='*' value='*' click={this.oper} />
-        </div>
-        <div className="row">
-          <Buttons display='1' value={1} click={this.numbers} />
-          <Buttons display='2' value={2} click={this.numbers} />
-          <Buttons display='3' value={3} click={this.numbers} />
-          <Buttons display='/' value='/' click={this.oper} />
-        </div>
-        <div className="row">
-          <Buttons display='0' value={0} click={this.numbers} />
-          <Buttons display='CE' click={this.clear} />
-          <Buttons display='=' value='=' click={this.Equation} />
-          <Buttons display='+' value='+' click={this.oper} />
-        </div>
+        <table className="table">
+          <tr>
+            <Buttons display='7' value={7} click={this.numbers} />
+            <Buttons display='8' value={8} click={this.numbers} />
+            <Buttons display='9' value={9} click={this.numbers} />
+            <Buttons display='-' value='-' click={this.oper} />
+          </tr>
+          <tr>
+            <Buttons display='4' value={4} click={this.numbers} />
+            <Buttons display='5' value={5} click={this.numbers} />
+            <Buttons display='6' value={6} click={this.numbers} />
+            <Buttons display='*' value='*' click={this.oper} />
+          </tr>
+          <tr>
+            <Buttons display='1' value={1} click={this.numbers} />
+            <Buttons display='2' value={2} click={this.numbers} />
+            <Buttons display='3' value={3} click={this.numbers} />
+            <Buttons display='/' value='/' click={this.oper} />
+          </tr>
+          <tr>
+            <Buttons display='0' value={0} click={this.numbers} />
+            <Buttons display='CE' click={this.clear} />
+            <Buttons display='=' value='=' click={this.Equation} />
+            <Buttons display='+' value='+' click={this.oper} />
+          </tr>
+        </table>
       </div>
     );
   }
