7826a2d627bd1ad00fc5b4e360ad1260064dbd00
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-04-27 15:25:45 -0400

    Update calculator.js

--- app/javascript/components/calculator.js
+++ app/javascript/components/calculator.js
@@ -1,11 +1,42 @@
 import React from "react"
-import PropTypes from "prop-types"
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      first_input: 0,
+      second_input: 0
+    }
+  }
+
   render () {
     return (
-      <React.Fragment>
+      <div>
         <h3>Hello from React gang</h3>
-      </React.Fragment>
+        <div className="row">
+          <Buttons display='7' value={7} click={this.numbers} />
+          <Buttons display='8' value={8} click={this.numbers} />
+          <Buttons display='9' value={9} click={this.numbers} />
+          <Buttons display='-' value='-' click={this.oper} />
+        </div>
+        <div className="row">
+          <Buttons display='4' value={4} click={this.numbers} />
+          <Buttons display='5' value={5} click={this.numbers} />
+          <Buttons display='6' value={6} click={this.numbers} />
+          <Buttons display='*' value='*' click={this.oper} />
+        </div>
+        <div className="row">
+          <Buttons display='1' value={1} click={this.numbers} />
+          <Buttons display='2' value={2} click={this.numbers} />
+          <Buttons display='3' value={3} click={this.numbers} />
+          <Buttons display='/' value='/' click={this.oper} />
+        </div>
+        <div className="row">
+          <Buttons display='0' value={0} click={this.numbers} />
+          <Buttons display='CE' click={this.clear} />
+          <Buttons display='=' value='=' click={this.Equation} />
+          <Buttons display='+' value='+' click={this.oper} />
+        </div>
+      </div>
     );
   }
 }
