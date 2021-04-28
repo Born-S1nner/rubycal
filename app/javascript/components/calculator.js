6a4de1d8abddab3239edbb48324e3de50dd42486
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-04-28 14:46:17 -0400

    Update calculator.js
    commit #1

--- app/javascript/components/calculator.js
+++ app/javascript/components/calculator.js
@@ -3,7 +3,7 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props)
     this.numbers = this.numbers.bind(this)
-    this.oper = this.oper.bind(this)
+    this.clear = this.clear.bind(this)
 
     this.onChangeInput = this.onChangeInput.bind(this)
 
@@ -16,17 +16,19 @@ class Calculator extends React.Component {
     if (this.state.first_input === 0) {
       let num = e.currentTarget.value;
       this.setState({
-        expression: num
+        first_input: num
       })
     } else {
       let num = this.state.first_input + e.currentTarget.value
       this.setState({
-        expression: num
+        first_input: num
       })
     }
   }
-  oper(e) {
-    console.log(e.currentTarget.value)
+  clear(e) {
+    this.setState({
+      first_input: 0
+    })
   }
   
   onChangeInput (event) {
@@ -45,44 +47,44 @@ class Calculator extends React.Component {
       />
     )
   }
+  tablet() {
+    return(
+      <table className="table">
+        <tr>
+          <Buttons display='7' value={7} click={this.numbers} />
+          <Buttons display='8' value={8} click={this.numbers} />
+          <Buttons display='9' value={9} click={this.numbers} />
+        </tr>
+        <tr>
+          <Buttons display='4' value={4} click={this.numbers} />
+          <Buttons display='5' value={5} click={this.numbers} />
+          <Buttons display='6' value={6} click={this.numbers} />
+        </tr>
+        <tr>
+          <Buttons display='1' value={1} click={this.numbers} />
+          <Buttons display='2' value={2} click={this.numbers} />
+          <Buttons display='3' value={3} click={this.numbers} />
+        </tr>
+        <tr>
+          <Buttons display='0' value={0} click={this.numbers} />
+          <Buttons display='CE' click={this.clear} />
+        </tr>
+      </table>
+    )
+  }
 
   render () {
     return (
       <div>
         <h3>Hello from React gang</h3>
         {this.renderInput('first_input', this.state.first_input)}
+        {this.tablet()}
         {this.renderInput('second_input', this.state.second_input)}
-        <table className="table">
-          <tr>
-            <Buttons display='7' value={7} click={this.numbers} />
-            <Buttons display='8' value={8} click={this.numbers} />
-            <Buttons display='9' value={9} click={this.numbers} />
-            <Buttons display='-' value='-' click={this.oper} />
-          </tr>
-          <tr>
-            <Buttons display='4' value={4} click={this.numbers} />
-            <Buttons display='5' value={5} click={this.numbers} />
-            <Buttons display='6' value={6} click={this.numbers} />
-            <Buttons display='*' value='*' click={this.oper} />
-          </tr>
-          <tr>
-            <Buttons display='1' value={1} click={this.numbers} />
-            <Buttons display='2' value={2} click={this.numbers} />
-            <Buttons display='3' value={3} click={this.numbers} />
-            <Buttons display='/' value='/' click={this.oper} />
-          </tr>
-          <tr>
-            <Buttons display='0' value={0} click={this.numbers} />
-            <Buttons display='CE' click={this.clear} />
-            <Buttons display='=' value='=' click={this.Equation} />
-            <Buttons display='+' value='+' click={this.oper} />
-          </tr>
-        </table>
       </div>
     );
   }
 }
 
-const Buttons = props => <button value={props.value} onClick={props.click}>{props.display}</button>
+const Buttons = props => <button value={props.value} onClick={props.click} id="button">{props.display}</button>
 
 export default Calculator
