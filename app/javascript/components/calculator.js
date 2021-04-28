972a4df572224e88a2e0ff7c2984d0a75cf1089d
Born-S1nner <62364929+Born-S1nner@users.noreply.github.com>
2021-04-28 15:11:52 -0400

    Update calculator.js

--- app/javascript/components/calculator.js
+++ app/javascript/components/calculator.js
@@ -2,8 +2,8 @@ import React from "react"
 class Calculator extends React.Component {
   constructor(props) {
     super(props)
-    this.numbers = this.numbers.bind(this)
-    this.clear = this.clear.bind(this)
+    this.firstNumbers = this.firstNumbers.bind(this)
+    this.firstClear = this.firstClear.bind(this)
 
     this.onChangeInput = this.onChangeInput.bind(this)
 
@@ -12,7 +12,7 @@ class Calculator extends React.Component {
       second_input: 0
     }
   }
-  numbers(e) {
+  firstNumbers(e) {
     if (this.state.first_input === 0) {
       let num = e.currentTarget.value;
       this.setState({
@@ -25,7 +25,7 @@ class Calculator extends React.Component {
       })
     }
   }
-  clear(e) {
+  firstClear(e) {
     this.setState({
       first_input: 0
     })
@@ -36,7 +36,6 @@ class Calculator extends React.Component {
       [event.target.name]: event.target.value
     })
   }
-
   renderInput(name, value) {
     return(
       <input
@@ -47,27 +46,27 @@ class Calculator extends React.Component {
       />
     )
   }
-  tablet() {
+  tablet(one, two) {
     return(
       <table className="table">
         <tr>
-          <Buttons display='7' value={7} click={this.numbers} />
-          <Buttons display='8' value={8} click={this.numbers} />
-          <Buttons display='9' value={9} click={this.numbers} />
+          <Buttons display='7' value={7} click={one} />
+          <Buttons display='8' value={8} click={one} />
+          <Buttons display='9' value={9} click={one} />
         </tr>
         <tr>
-          <Buttons display='4' value={4} click={this.numbers} />
-          <Buttons display='5' value={5} click={this.numbers} />
-          <Buttons display='6' value={6} click={this.numbers} />
+          <Buttons display='4' value={4} click={one} />
+          <Buttons display='5' value={5} click={one} />
+          <Buttons display='6' value={6} click={one} />
         </tr>
         <tr>
-          <Buttons display='1' value={1} click={this.numbers} />
-          <Buttons display='2' value={2} click={this.numbers} />
-          <Buttons display='3' value={3} click={this.numbers} />
+          <Buttons display='1' value={1} click={one} />
+          <Buttons display='2' value={2} click={one} />
+          <Buttons display='3' value={3} click={one} />
         </tr>
         <tr>
-          <Buttons display='0' value={0} click={this.numbers} />
-          <Buttons display='CE' click={this.clear} />
+          <Buttons display='0' value={0} click={one} />
+          <Buttons display='CE' click={two} />
         </tr>
       </table>
     )
@@ -78,7 +77,7 @@ class Calculator extends React.Component {
       <div>
         <h3>Hello from React gang</h3>
         {this.renderInput('first_input', this.state.first_input)}
-        {this.tablet()}
+        {this.tablet(this.firstNumbers, this.firstClear)}
         {this.renderInput('second_input', this.state.second_input)}
       </div>
     );
